ac487b6f075679aae1c17a4c2b2a04737367b130
Luisa Martins <Luisa.Andre.Martins@formandos.cinel.pt>
2020-12-17 16:56:42 +0000

    Inicialização do projecto

---
+++ LojaCet50B/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LojaCet50B.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
