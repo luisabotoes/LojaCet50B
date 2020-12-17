c8251bca422e147ca2ad41522006cb506598c1ab
Luisa Martins <Luisa.Andre.Martins@formandos.cinel.pt>
2020-12-17 19:08:30 +0000

    CRUD de produtos para a web

--- LojaCet50B/Dados/Entidades/Produto.cs
+++ LojaCet50B/Dados/Entidades/Produto.cs
@@ -10,7 +10,7 @@ namespace LojaCet50B.Dados.Entidades
 
         public string Nome { get; set; }
 
-
+        [Display(Name = "Preço")]
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Preco { get; set; }
 
