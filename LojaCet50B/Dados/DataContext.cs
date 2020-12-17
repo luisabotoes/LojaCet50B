fb83b9013dc022863ef7768ccb50882ca1587351
Luisa Martins <Luisa.Andre.Martins@formandos.cinel.pt>
2020-12-17 18:42:53 +0000

    Criar a base de dados e tabela Produtos na loja B

---
+++ LojaCet50B/Dados/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using LojaCet50B.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaCet50B.Dados
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
--- LojaCet50B/Startup.cs
+++ LojaCet50B/Startup.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using LojaCet50B.Dados;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LojaCet50B
 {
@@ -24,6 +21,12 @@ namespace LojaCet50B
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
